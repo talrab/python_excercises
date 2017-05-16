1350dfba6f819e8a269347ab0d1fd79899996557
Tal <talrab@gmail.com>
2017-05-16 12:11:11 +0300

    file created

--- findSumInUnSortedList.py
+++ findSumInUnSortedList.py
@@ -11,6 +11,14 @@ target = 110
 reminder={}
 answer=()
 
+# what we do basically is maintaining a dictionary
+# with the reminder between the target and the list
+# element as the key and the index as the value
+# so for a target of 110 and the first list element as 10
+# the first dictionary element will be 100:0
+# while we are building this dictionary of reminders we
+# caluclate a needed reminder and check whether it is in our dictionary
+# if it is , we have our answer, if not we add it
 for i, n in enumerate(a):
     r = target-n
     if n in reminder:
