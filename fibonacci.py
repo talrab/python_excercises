60bad7619dbb2ad1e5b1326fbabb5588f12823aa
Tal <talrab@gmail.com>
2017-05-17 14:04:58 +0300

    changes

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,10 @@
 
-while (True):
-    num_elements = int(input("Please enter the number of elements: "))
+
+run_loop = True
+num_elements = int(input("Please enter the number of elements: "))
+if num_elements < 0:
+    run_loop = False
+while (run_loop):
     answer = []
 
     for x in range(num_elements):
@@ -13,3 +17,24 @@ while (True):
             answer.append (answer[x-2] + answer[x-1])
 
     print (answer)
+    num_elements = int(input("Please enter the number of elements: "))
+    if num_elements < 0:
+        run_loop = False
+
+
+
+
+# a 'classier' solution to find the nth element of fibonacchi:
+def fib(n):
+    a,b = 0,1
+    for i in range(n):
+        a,b = b,b+a
+    return a
+
+print(fib(5))
+
+
+
+
+
+
